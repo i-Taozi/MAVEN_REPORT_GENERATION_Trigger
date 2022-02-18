94835286949d0081e46aa6e75964abacf97de9d3
i-Taozi <879714748@qq.com>
2022-02-19 00:24:46 +0800

    upyun/java-sdk:original code

--- src/main/java/org/example/kickoff/business/exception/EntityAlreadyModifiedException.java
+++
@@ -1,7 +0,0 @@
-package org.example.kickoff.business.exception;
-
-public class EntityAlreadyModifiedException extends EntityException {
-
-	private static final long serialVersionUID = 1L;
-
-}
--- src/main/java/org/example/kickoff/business/exception/EntityException.java
+++
@@ -1,7 +0,0 @@
-package org.example.kickoff.business.exception;
-
-public abstract class EntityException extends BusinessException {
-
-	private static final long serialVersionUID = 1L;
-
-}
